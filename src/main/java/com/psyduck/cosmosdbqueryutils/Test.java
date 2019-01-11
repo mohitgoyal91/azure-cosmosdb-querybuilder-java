cc406331adfb9e9547687ac0fba3dc1fab94552d
mohitgoyal91 <mohitgoyal91@yahoo.co.in>
2019-01-12 03:46:56 +0530

    Sample Test File with main method to test

---
+++ src/main/java/com/psyduck/cosmosdbqueryutils/Test.java
@@ -0,0 +1,14 @@
+package com.psyduck.cosmosdbqueryutils;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Test {
+
+    public static void main(String args[]) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        System.out.println("My First Query: " + getSelectQuery());
+    }
+
+    private static String getSelectQuery() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        return new SelectQuery<>().createQuery();
+    }
+}
