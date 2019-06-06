ef0a0f08ee978101d42d860d56b74c5e345e475a
mohitgoyal91 <mohitgoyal91@yahoo.co.in>
2019-06-07 01:50:19 +0530

    ** jUnit integration

--- src/test/java/com/github/mohitgoyal91/cosmosdbqueryutils/SelectQueryTest.java
+++ src/test/java/com/github/mohitgoyal91/cosmosdbqueryutils/SelectQueryTest.java
@@ -1,6 +1,5 @@
 package com.github.mohitgoyal91.cosmosdbqueryutils;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
